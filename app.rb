a397132b527e467ef86d9cc50613768845e45202
ARUN A MENON <52127560+arunamenon@users.noreply.github.com>
2024-12-14 18:11:17 +0000

    updates

--- app.rb
+++ app.rb
@@ -1,9 +1,62 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = Math.sqrt(@the_num)
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get("/payment/results") do
+  @principal = params.fetch("principal").to_f
+  @apr = params.fetch("apr").to_f / 100
+  @years = params.fetch("years").to_f
+
+  monthly_rate = @apr / 12
+  months = @years * 12
+  @monthly_payment = (@principal * monthly_rate) / (1 - (1 + monthly_rate)**-months)
+
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:new_random_calc)
+end
+
+get("/random/results") do
+  @min = params.fetch("min").to_i
+  @max = params.fetch("max").to_i
+  @random_number = rand(@min..@max)
+  erb(:random_results)
+end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  "<h1>Welcome to your Sinatra App!</h1>
+   <p>Define some routes in app.rb</p>"
 end
